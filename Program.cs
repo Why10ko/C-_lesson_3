6f7c0e23fabf5cf1ba15a4356a7ebf33d956a0d4
Yurii Voytenko <why10ko@yandex.ru>
2023-12-05 10:38:15 +0300

    C# lesson 3

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_1
+
+// Задача 1 -------------------------------------------
+
+{
+	public class Program
+	{
+		public static void Main(string [] arg) 
+		{
+			Random rand = new Random();
+			int size = 10;			
+			int [] array = new int[size];
+
+			for (int i = 0; i < size; i++) 
+			{
+				array[i] = rand.Next(0,100);
+			}
+
+			foreach (int item in array) 
+			{
+				System.Console.Write(item + " ");
+			}			
+		}
+	}
+}
+
+// Задача 2 -------------------------------------------
+// Задача 3 -------------------------------------------
+// Задача 4 -------------------------------------------
