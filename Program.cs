f131b5c5e3f987533e6f30253e35944b7babae60
Yurii Voytenko <why10ko@yandex.ru>
2023-12-05 10:55:40 +0300

    C# exercise 2, even numbers

--- Program.cs
+++ Program.cs
@@ -2,6 +2,37 @@
 
 // Задача 1 -------------------------------------------
 
+// {
+// 	public class Program
+// 	{
+// 		public static void Main(string [] arg) 
+// 		{
+// 			Random rand = new Random();
+// 			int size = 10;			
+// 			int [] array = new int[size];
+// 			int total = 0;
+
+// 			for (int i = 0; i < size; i++) 
+// 			{
+// 				array[i] = rand.Next(0,100);
+// 				if (array[i] > 19 && array[i] < 91) 
+// 				{
+// 					total++;
+// 				}
+// 			}
+
+// 			foreach (int item in array) 
+// 			{
+// 				System.Console.Write(item + " ");
+// 			}		
+
+// 			System.Console.WriteLine($"There are {total} numbers in total less than 90 and more then 19 ");
+// 		}
+// 	}
+// }
+
+// Задача 2 -------------------------------------------
+
 {
 	public class Program
 	{
@@ -15,7 +46,7 @@
 			for (int i = 0; i < size; i++) 
 			{
 				array[i] = rand.Next(0,100);
-				if (array[i] > 19 && array[i] < 91) 
+				if (array[i] % 2 == 0) 
 				{
 					total++;
 				}
@@ -26,14 +57,10 @@
 				System.Console.Write(item + " ");
 			}		
 
-			System.Console.WriteLine($"There are {total} numbers in total less than 90 and more then 19 ");
+			System.Console.WriteLine($"There are {total} even numbers in the array ");
 		}
 	}
 }
-
-// Задача 2 -------------------------------------------
-
-
 
 // Задача 3 -------------------------------------------
 // Задача 4 -------------------------------------------
